6624f9c126fa6a7b99113d772f3c31ed1c0c222e
sailesh <sailesh@jobins.jp>
2024-04-20 15:35:41 +0545

    added cors

--- server/app.js
+++ server/app.js
@@ -1,10 +1,19 @@
 const dotenv = require("dotenv");
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 dotenv.config({ path: "./config.env" });
 require("./db/conn");
 // const User = require("./model/userSchema");
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://mern-login-sever.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
@@ -17,9 +26,9 @@ const middleware = (req, res, next) => {
   next();
 };
 
-// app.get("/", (req, res) => {
-//   res.send(`hello world from server app.js`);
-// });
+app.get("/", (req, res) => {
+  res.json(`hello world from server app.js`);
+});
 
 app.get("/about", middleware, (req, res) => {
   res.send(`hello world from about`);
