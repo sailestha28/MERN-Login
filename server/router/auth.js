9ac0e589ba4f3e48b9637c2229e83e449794b011
sailesh <sailesh@jobins.jp>
2024-04-20 14:18:56 +0545

    feat: user register using async

--- server/router/auth.js
+++ server/router/auth.js
@@ -10,37 +10,31 @@ router.get("/", (req, res) => {
   res.send(`hello world from server router js`);
 });
 
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
   const { name, email, phone, work, password, confirmPassword } = req.body;
   if (!name || !email || !phone || !work || !password || !confirmPassword) {
     return res.status(422).json({ error: "all field should be entered" });
   }
-  User.findOne({ email: email })
-    .then((userExist) => {
-      if (userExist) {
-        return res.status(422).json({ error: "Email already exits" });
-      }
-      const user = new User({
-        name,
-        email,
-        phone,
-        work,
-        password,
-        confirmPassword,
-      });
+  try {
+    const userExist = await User.findOne({ email: email });
 
-      user
-        .save()
-        .then(() => {
-          res.status(201).json({ message: "User register successful" });
-        })
-        .catch((err) => {
-          err.status(500).json({ message: "Failed to registered" });
-        });
-    })
-    .catch((err) => {
-      console.log(err);
+    if (userExist) {
+      return res.status(422).json({ error: "Email already exits" });
+    }
+    const user = new User({
+      name,
+      email,
+      phone,
+      work,
+      password,
+      confirmPassword,
     });
+
+    await user.save();
+    res.status(201).json({ message: "User register successful" });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
